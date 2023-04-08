de56ac2758624808cfef31e2ba23f9113e2ab1f1
Fran Cudina <cudina.fran@gmail.com>
2023-04-08 23:49:44 +0200

    servo calibration script

---
+++ Projects/AutonomousDriving/servo_calibration.py
@@ -0,0 +1,30 @@
+import sys
+
+from Projects.AutonomousDriving.Services.Driving.DrivingUtils import DrivingUtils
+from Projects.Executables.External.Motors.MotorConfig import MotorConfig
+from Projects.Executables.External.Motors.ServoSG90 import ServoSG90
+
+"""
+    Servo Calibration: used to reset servo angle to starting position.
+"""
+if __name__ == '__main__':
+
+    # expecting: <servo_pin_number> <board_mode>
+    args = sys.argv[1:]
+
+    print(f"# Starting servo on pin: {args[0]} with board mode: {args[1]}")
+    servo: ServoSG90 = ServoSG90(
+        pin_number=int(args[0]),
+        board_mode=DrivingUtils.get_board_mode(args[1])
+    )
+    servo.start()
+    print("> servo started")
+
+    # set servo angle
+    print("# starting with calibration...")
+    servo.new_result(input_angle=MotorConfig.SERVO_STARTING_POINT_ANGLE.value)
+    print("> done")
+
+    print("# servo cleanup...")
+    servo.stop()
+    print("> done")
--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Repository used as a playground for Raspberry Pi ideas
     # enter path to configuration file, e.g.:
     Projects/AutonomousDriving/driving_input.json
     ```
+  
+- servo calibration (from `root` dir):
+    ```
+    # <servo_pin_number> <board_mode> are input params, e.g.: 4 GPIO.BCM
+    python3 -m Projects.AutonomousDriving.servo_calibration <servo_pin_number_param> <board_mode_param>
+    ```
