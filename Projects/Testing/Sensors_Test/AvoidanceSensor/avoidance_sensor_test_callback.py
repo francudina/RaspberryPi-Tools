3c6cbbaa2fffe311264599e064626481b0ce2bfe
Fran Cudina <cudina.fran@gmail.com>
2023-04-09 20:34:30 +0200

    avoidance sensor testing with callback

---
+++ Projects/Testing/Sensors_Test/AvoidanceSensor/avoidance_sensor_test_callback.py
@@ -0,0 +1,38 @@
+import sys
+from time import sleep
+from datetime import datetime
+
+import RPi.GPIO as GPIO
+
+
+def event_detected(channel):
+    state = GPIO.input(channel)
+    if state == GPIO.LOW:
+        print(f"\t+ event detected on channel '{channel}' at {datetime.utcnow().strftime('%H:%M:%S.%f')}")
+
+
+def setup(obstacle_sensor_pin: int):
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(obstacle_sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    GPIO.add_event_detect(obstacle_sensor_pin, GPIO.FALLING, callback=event_detected, bouncetime=250)
+
+
+def loop():
+    while True:
+        sleep(0.01)
+            
+
+if __name__ == '__main__':
+
+    args = sys.argv[1:]
+    obstacle_sensor_pin = int(args[0])
+
+    setup(obstacle_sensor_pin)
+    try:
+        loop()
+    except Exception:
+        GPIO.remove_event_detect(obstacle_sensor_pin)
+        GPIO.cleanup()
+
--- RPi/GPIO.py
+++ RPi/GPIO.py
@@ -18,6 +18,9 @@ LOW = 0
 RISING = 1
 FALLING = 0
 
+PUD_UP = 1
+PUD_DOWN = 0
+
 
 class PwdTmp:
 
@@ -35,8 +38,8 @@ def setmode(mode=None):
     print("setmode:", mode)
 
 
-def setup(a: int, b=None):
-    print("setup:", a, b)
+def setup(a: int, b=None, pull_up_down=None):
+    print("setup:", a, b, pull_up_down)
 
 
 def input(a: int, b=None):
@@ -56,9 +59,13 @@ def setwarnings(flag: bool=None):
     print("setwarnings:", flag)
 
 
-def add_event_detect(a=None, b=None, callback=None):
-    print("add_event_detect triggered!")
+def add_event_detect(a=None, b=None, callback=None, bouncetime=None):
+    print("add_event_detect setup: ", a, b, callback, bouncetime)
 
 
-def PWM(a: int, b:int):
+def remove_event_detect(a=None):
+    print("remove_event_detect setup: ", a)
+
+
+def PWM(a: int, b: int):
     return PwdTmp()
