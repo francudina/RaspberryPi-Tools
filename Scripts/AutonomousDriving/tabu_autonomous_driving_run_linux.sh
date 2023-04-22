3b3702719c324459c5ac4569ac9110c0fe4d20cd
Fran Cudina <cudina.fran@gmail.com>
2023-04-22 20:37:05 +0200

    sh script update

---
+++ Scripts/AutonomousDriving/tabu_autonomous_driving_run_linux.sh
@@ -0,0 +1,4 @@
+python3 -m Projects.AutonomousDriving.driving_script \
+--input algorithm \
+--config /home/pi/Documents/raspberry-pi-documents/Projects/AutonomousDriving/device_config.json \
+--algorithm tabu_search
